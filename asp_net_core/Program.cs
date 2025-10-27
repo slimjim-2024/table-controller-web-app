c48ffca3b10e14ba50a7d8bc65b0975cb60f60db
Ktoto198 <frybenderer43@gmail.com>
2025-10-27 15:37:08 +0100

    added mysql support

--- asp_net_core/Program.cs
+++ asp_net_core/Program.cs
@@ -9,7 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseMySQL(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.AddRazorComponents();
 
