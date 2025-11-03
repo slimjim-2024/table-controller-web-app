6d740df3ca98eb94f2f10546797deaf348edc95f
Owl <balazs200428@gmail.com>
2025-11-03 19:02:45 +0100

    Dark mode updated with forms and browser settings

--- asp_net_core/wwwroot/js/site.js
+++ asp_net_core/wwwroot/js/site.js
@@ -1,21 +1,33 @@
-﻿darkModeCheck.addEventListener("click", makeDarkmode);
+﻿document.addEventListener("DOMContentLoaded", () => {
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        document.body.classList.toggle("darkMode");
 
-let interacted = false;
+        document.getElementById("darkModeCheck").checked = true;
 
-function makeDarkmode()
-{
+        var sliderImg = document.getElementById("sliderIMG");
+
+        sliderImg.src = "/moony.svg";
+        sliderImg.classList.add("animate");
+        interacted = true;
+    }
+});
+
+darkModeCheck.addEventListener("click", () => {
     document.body.classList.toggle("darkMode");
 
     var sliderImg = document.getElementById("sliderIMG");
+
+    if (document.getElementById("darkModeCheck").checked) {
+        sliderImg.src = "/moony.svg";
+    }
+    else {
+        sliderImg.src = "/sunny.svg";
+    }
 
     if (!interacted) {
         sliderImg.classList.add("animate");
         interacted = true;
     }
-    if (document.getElementById("darkModeCheck").checked) {
-        sliderImg.src = "/moony.png";
-    }
-    else {
-        sliderImg.src = "/sunny.png";
-    }
-}
+});
+
+let interacted = false;
